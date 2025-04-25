640b85366881b9d939300aea11e0ba6d53a546e2
anand5206 <ananddeepakjadhav@gmail.com>
2025-04-25 10:18:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
         System.out.println("Hello, World!");
     }
 }
